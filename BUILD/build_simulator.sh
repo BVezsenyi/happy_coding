721d8794c4b48f750ebc962dc23f1a9bb03c0cb9
Balint <vezsenyibalint@gmail.com>
2019-10-31 23:23:57 +0100

    Docker command path fixation

--- BUILD/build_simulator.sh
+++ BUILD/build_simulator.sh
@@ -1,1 +1,1 @@
-docker run -ti --rm -v "$(pwd)":/work/Dev stronglytyped/arm-none-eabi-gcc /bin/bash -c "cd Dev; make -f application.mk simulator"
+docker run -ti --rm -v "$(pwd)/../":/work/Dev stronglytyped/arm-none-eabi-gcc /bin/bash -c "cd Dev; make -f application.mk simulator"
