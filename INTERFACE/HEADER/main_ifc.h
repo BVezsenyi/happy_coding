2c206479456cf7205c18871f82f40625bc5fe7d5
Balint <vezsenyibalint@gmail.com>
2019-11-09 15:31:58 +0100

    Argument is fixed

--- INTERFACE/HEADER/main_ifc.h
+++ INTERFACE/HEADER/main_ifc.h
@@ -4,7 +4,7 @@
 #include "common_defines.h"
 #include "hw_api.h"
 
-void execute_function( FN_PTR_LIST );
+void execute_function( FN_ID_LIST );
 void ERROR_HANDLER( const char* , const char* );
 
 #endif /* _MAIN_IFC_H_ */
