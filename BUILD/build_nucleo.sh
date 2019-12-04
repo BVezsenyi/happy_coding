efd6adcc3fa87abc804db29efc3b7a10b85a1f5a
Balint <vezsenyibalint@gmail.com>
2019-12-04 20:39:55 +0100

    Build improvement
    From now there is one machine for cleaning and for compiling simulator

--- BUILD/build_nucleo.sh
+++ BUILD/build_nucleo.sh
@@ -4,11 +4,21 @@ DOCKERFILE=../DOCKER/ARM/Dockerfile
 IMG_NAME="arm_gcc_img"
 IMG_RET=`docker images | grep $IMG_NAME`
 
-if [ "$IMG_RET" = "" ]; then
-    echo "$IMG_NAME: Image does not exist. It should be generated"
-    docker build -f $DOCKERFILE -t $IMG_NAME .
-else 
-    echo "$IMG_NAME is exist."
-fi;
+function build_docker_image()
+{
+    if [ "$IMG_RET" = "" ]; then
+        echo "$IMG_NAME: Image does not exist. It should be generated"
+        docker build -f $DOCKERFILE -t $IMG_NAME .
+    else 
+        echo "$IMG_NAME is exist."
+    fi;
+}
 
-docker run -ti --rm -v "$(pwd)/../":/work/Dev $IMG_NAME /bin/bash -c "cd Dev; make -f application.mk nucleo"
+function run_build_in_docker()
+{
+    docker run -ti --rm -v "$(pwd)/../":/work/Dev $IMG_NAME /bin/bash -c "cd Dev; make -f application.mk nucleo"
+}
+
+#Main: 
+build_docker_image
+run_build_in_docker
--- BUILD/build_simulator_and_clean.sh
+++ BUILD/build_simulator_and_clean.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DOCKERFILE=../DOCKER/AVR/Dockerfile
-IMG_NAME="avr_gcc_img"
+DOCKERFILE=../DOCKER/SIMULATOR/Dockerfile
+IMG_NAME="simulator_and_clean_img"
 IMG_RET=`docker images | grep $IMG_NAME`
+TARGET=$1
 
 function build_docker_image()
 {
     if [ "$IMG_RET" = "" ]; then
         echo "$IMG_NAME: Image does not exist. It should be generated"
         docker build -f $DOCKERFILE -t $IMG_NAME .
-
     else 
         echo "$IMG_NAME is exist."
     fi;
@@ -17,7 +17,12 @@ function build_docker_image()
 
 function run_build_in_docker()
 {
-    docker run -ti --rm -v "$(pwd)/../":/work/Dev $IMG_NAME /bin/bash -c "cd Dev; make -f application.mk tbird"
+    if [ "$TARGET" == "simulator" ] || [ "$TARGET" == "clean" ]; then
+        docker run -ti --rm -v "$(pwd)/../":/work/Dev $IMG_NAME /bin/bash -c "cd Dev; make -f application.mk $TARGET"
+    else   
+        echo "Invaldi argument exception"
+        exit
+    fi;
 }
 
 
