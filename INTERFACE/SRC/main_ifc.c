5484e9a4673faede3ccceb1dd349ba9ef37a17ce
Balint <vezsenyibalint@gmail.com>
2019-12-05 20:13:15 +0100

    Name fixations

--- INTERFACE/SRC/main_ifc.c
+++ INTERFACE/SRC/main_ifc.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main_ifc.h"
 
-static void  check_fn_id_validity( FUNCTION_ID_LIST );
+static void  check_fn_id_validity( FUNCTION_ID );
 
-void* get_function_pointer_by_id( FUNCTION_ID_LIST fn_id )
+void* get_function_pointer_by_id( FUNCTION_ID fn_id )
 {
     void* fn_ptr_ret_val = INVALID_FN_PTR;
     
@@ -19,7 +19,7 @@ void* get_function_pointer_by_id( FUNCTION_ID_LIST fn_id )
     return fn_ptr_ret_val;
 }
 
-static void check_fn_id_validity( FUNCTION_ID_LIST fn_id )
+static void check_fn_id_validity( FUNCTION_ID fn_id )
 {    
     if( ( FN_INIT > fn_id ) || ( FN_MAX_VALUE <= fn_id ) )
     {
